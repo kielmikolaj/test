33e0a083e9b9b211b433bd38f6497c3e3d4d6630
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-10 10:51:59 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,33 +1,36 @@
 import groovy.json.*
 
 
-stage('Bump API version in bom') 
-{
+//stage('Bump API version in json') 
+//{
             
             def jsonSlurper = new JsonSlurper()
             config = jsonSlurper.parse(new File('package.json'))
+            println(config
             
             
             
-            steps 
-            {
-                script 
-                {
-                    dir('ccb-bom-modules') 
-                    {
-                        git branch: "$params.UPDATE_BOM_BRANCH",
-                                credentialsId: 'fbujenkinsccbref',
-                                url: 'ssh://git@fbu-git.krakow.comarch:2224/ccb-ref/backend/components/ccb-bom-modules.git'
-                        sh """mvn org.codehaus.mojo:versions-maven-plugin:2.5:set-property \
-                              -DallowDowngrade=false -DgenerateBackupPoms=false -DallowSnapshots=false \
-                              -DnewVersion='${newVersion}' -Dproperty='ccb-frontend-api-ib.version'"""
+            //steps 
+           // {
+               // script 
+              //  {
+                  //  dir('test') 
+                  //  {
+                       // git branch: "$params.UPDATE_BOM_BRANCH",
+                                //credentialsId: 'fbujenkinsccbref',
+                            //    url: 'https://github.com/kielmikolaj/test.git'
+                                
+                                
+                      
+                                
+                                
                        /* sh "git config user.name ${GIT_COMMITTER_NAME}"
                         sh "git config user.email ${GIT_COMMITTER_EMAIL}"
                         sh """git commit -am 'bump ccb-frontend-api-ib to ${newVersion}'"""
                         sshagent(credentials: ['fbujenkinsccbref']) {
                             sh "git push origin ${params.UPDATE_BOM_BRANCH}" 
                         } */
-                    }
-                }
-            }
-}
+                  //  }
+              //  }
+          //  }
+//}
