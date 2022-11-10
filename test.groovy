5fd66a9cdd930baa4c7d823edc943c1caf1349ef
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-10 13:50:03 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -16,7 +16,7 @@ pipeline
                         //def config = jsonSlurper.parse(new File('package.json'))
                         def inputFile = readFile(file: "package.json")
                         def InputJSON = new JsonSlurper().parseText(inputFile)
-                        println(InputJSON)
+                        println(InputJSON.toPrettyString())
                     }
                }
             }
