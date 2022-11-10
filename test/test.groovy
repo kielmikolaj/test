abb1ce3104cf42d563805fb06d05d06b632928dc
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-10 12:34:11 +0100

    Create test.groovy

---
+++ test/test.groovy
@@ -0,0 +1,25 @@
+import groovy.json.*
+
+pipeline
+{
+    agent any
+
+    stages {
+        stage('Bump API version in json') {
+            steps {
+                dir('test'){
+                     git branch: 'main',
+                     url: 'https://github.com/kielmikolaj/test.git'
+
+                    script{
+                        //def jsonSlurper = new JsonSlurper()
+                        //def config = jsonSlurper.parse(new File('package.json'))
+                        def inputFile = readFile(file: "package.json")
+                        def InputJSON = new JsonSlurper().parseText(inputFile)
+                        println(InputJSON)
+                    }
+               }
+            }
+        }
+    }
+}
