1a23c972e65c1a56f017886cd2564c9f28d64eba
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-10 11:01:31 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,36 +1,42 @@
 import groovy.json.*
 
 
-//stage('Bump API version in json') 
-//{
+stage('Bump API version in json') 
+{
             
-            def jsonSlurper = new JsonSlurper()
-            config = jsonSlurper.parse(new File('./package.json'))
-            println(config)
+           
             
+            steps {
+                        script{
+                                    dir('test'){
+                                                 def jsonSlurper = new JsonSlurper()
+                                                def config = jsonSlurper.parse(new File('./package.json'))
+                                                println(config)
+                                    }
+                        }
+            }
             
-            
-            //steps 
-           // {
-               // script 
-              //  {
-                  //  dir('test') 
-                  //  {
-                       // git branch: "$params.UPDATE_BOM_BRANCH",
-                                //credentialsId: 'fbujenkinsccbref',
-                            //    url: 'https://github.com/kielmikolaj/test.git'
+//             steps 
+//             {
+//                 script 
+//                 {
+//                     dir('test') 
+//                     {
+//                        // git branch: "$params.UPDATE_BOM_BRANCH",
+//                                 //credentialsId: 'fbujenkinsccbref',
+//                             //    url: 'https://github.com/kielmikolaj/test.git'
                                 
                                 
                       
                                 
                                 
-                       /* sh "git config user.name ${GIT_COMMITTER_NAME}"
-                        sh "git config user.email ${GIT_COMMITTER_EMAIL}"
-                        sh """git commit -am 'bump ccb-frontend-api-ib to ${newVersion}'"""
-                        sshagent(credentials: ['fbujenkinsccbref']) {
-                            sh "git push origin ${params.UPDATE_BOM_BRANCH}" 
-                        } */
-                  //  }
-              //  }
-          //  }
-//}
+//                        /* sh "git config user.name ${GIT_COMMITTER_NAME}"
+//                         sh "git config user.email ${GIT_COMMITTER_EMAIL}"
+//                         sh """git commit -am 'bump ccb-frontend-api-ib to ${newVersion}'"""
+//                         sshagent(credentials: ['fbujenkinsccbref']) {
+//                             sh "git push origin ${params.UPDATE_BOM_BRANCH}" 
+//                         } */
+//                   //  }
+//               //  }
+//           //  }
+// //}
