0b8b5872c5b38377b9fc9c183ac18c1020749451
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-15 12:36:32 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -17,8 +17,8 @@ pipeline
                         def inputFile = readFile(file: "package.json")
                         //def InputJSON = new JsonSlurper().parseText(inputFile)
                         // println(inputFile)
-                        inputFile['ccb-frontend-apispecification-ib'] = 'nowa wersja'
-                        writeJSON file: 'package.json', json: inputFile
+                        inputFile["ccb-frontend-apispecification-ib"] = 'nowa wersja'
+                        writeJSON file: "package.json", json: inputFile
                         // def jbuilder = new JsonBuilder(InputJSON)
                         // jbuilder.inputFile.dependencies."ccb-frontend-apispecification-ib" = "nowa wersja"
                         // println(jbuilder)
