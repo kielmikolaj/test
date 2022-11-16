ea0f01e420f99474a5ecd314a1657ca222bc8fe7
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-16 12:09:10 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -15,13 +15,13 @@ pipeline
                         
                         //def config = jsonSlurper.parse(new File('package.json'))
                         def inputFile = readFile(file: "package.json")
-                        def InputJSON = new JsonSlurper().parseText(inputFile)
+                        //def InputJSON = new JsonSlurper().parseText(inputFile)
                         // println(inputFile)
-                        //inputFile["wersja"] = 'nowa wersja'
-                        //writeJSON file: "package.json", json: inputFile
-                        def jbuilder = new JsonBuilder(InputJSON)
-                        jbuilder.dependencies.wersja = "nowa wersja"
-                        println(jbuilder)
+                        inputFile["wersja"] = 'nowa wersja'
+                        writeJSON file: "package.json", json: inputFile
+                        //def jbuilder = new JsonBuilder(InputJSON)
+                        //jbuilder.dependencies.wersja = "nowa wersja"
+                        //println(jbuilder)
                         inputFile = null
 
                     }
