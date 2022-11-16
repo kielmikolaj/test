3026f98bf3eb615a8a1e3d7864ea53c9e502680f
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-16 14:11:09 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,5 +1,5 @@
 import groovy.json.*
-/*
+
 pipeline
 {
     agent any
@@ -12,35 +12,8 @@ pipeline
                      url: 'https://github.com/kielmikolaj/test.git'
 
                     script{
-                        
-                        //def config = jsonSlurper.parse(new File('package.json'))
-                        def jsonFile = readFile(file: "package.json")
-                        def jsonSlurper = new JsonSlurper().parseText(jsonFile)
-                        //def versionFind = jsonSlurper.dependencies.find { it.name == "ccb-frontend-apispecification-ib" }
-                        //version.value."ccb-frontend-apispecification-ib" = "nowa wersja"
-                        //println new JsonBuilder(jsonSlurper).toPrettyString()
-                        // println(inputFile)
-                        //inputFile["wersja"] = 'nowa wersja'
-                        
-                        def jbuilder = new JsonBuilder(jsonSlurper)
-                        jbuilder."dependencies"."ccb-frontend-apispecification-ib" = "nowa wersja"
-                        writeJSON file: "package.json", json: jsonFile
-                        //println(jbuilder)
-                        jsonFile = null
-
-                    }
-               }
-            }
-        }
-    }
-}
-*/
-
-
-
-
-
-def content = """
+                    
+                    def content = """
 {
    "app":{ },
    "at":2,
@@ -61,3 +34,32 @@ def builder = new JsonBuilder(slurped)
 builder.content.device.dpidsha1 = 'abcd'  
 println(builder.toPrettyString())
 writeJSON file: "package2.json", json: content
+                        /*
+                        //def config = jsonSlurper.parse(new File('package.json'))
+                        def jsonFile = readFile(file: "package.json")
+                        def jsonSlurper = new JsonSlurper().parseText(jsonFile)
+                        //def versionFind = jsonSlurper.dependencies.find { it.name == "ccb-frontend-apispecification-ib" }
+                        //version.value."ccb-frontend-apispecification-ib" = "nowa wersja"
+                        //println new JsonBuilder(jsonSlurper).toPrettyString()
+                        // println(inputFile)
+                        //inputFile["wersja"] = 'nowa wersja'
+                        
+                        def jbuilder = new JsonBuilder(jsonSlurper)
+                        jbuilder."dependencies"."ccb-frontend-apispecification-ib" = "nowa wersja"
+                        writeJSON file: "package.json", json: jsonFile
+                        //println(jbuilder)
+                        jsonFile = null
+*/
+                    }
+               }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
