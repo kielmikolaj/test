51d9a73e1acea2795e1a6186202c032bbc281482
kielmikolaj <109137335+kielmikolaj@users.noreply.github.com>
2022-11-22 09:49:52 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -13,44 +13,21 @@ pipeline
 
                     script{
                     
-                    def content = """
-{
-   "app":{ },
-   "at":2,
-   "badv":[ ],
-   "bcat":[ ],
-   "device":{
-      "carrier":"310-410",
-      "connectiontype":3,
-      "devicetype":1,
-      "dnt":0,
-      "dpidmd5":"268d403db34e32c45869bb1401247af9",
-      "dpidsha1":"1234" 
-   }
-}"""
-
-def slurped = new JsonSlurper().parseText(content)
-def builder = new JsonBuilder(slurped)
-builder.content.device.dpidsha1 = 'abcd'  
-println(builder.toPrettyString())
-writeJSON file: "package2.json", json: content
-                        builder = null
-                        /*
                         //def config = jsonSlurper.parse(new File('package.json'))
-                        def jsonFile = readFile(file: "package.json")
-                        def jsonSlurper = new JsonSlurper().parseText(jsonFile)
+                        jsonFile = readJSON file: 'package.json'
+                        //def jsonSlurper = new JsonSlurper().parseText(jsonFile)
                         //def versionFind = jsonSlurper.dependencies.find { it.name == "ccb-frontend-apispecification-ib" }
                         //version.value."ccb-frontend-apispecification-ib" = "nowa wersja"
                         //println new JsonBuilder(jsonSlurper).toPrettyString()
                         // println(inputFile)
-                        //inputFile["wersja"] = 'nowa wersja'
+                        jsonFile['dependencies']['ccb-frontend-apispecification-ib'] = 'nowa wersja'
                         
-                        def jbuilder = new JsonBuilder(jsonSlurper)
-                        jbuilder."dependencies"."ccb-frontend-apispecification-ib" = "nowa wersja"
+                        //def jbuilder = new JsonBuilder(jsonSlurper)
+                        //jbuilder."dependencies"."ccb-frontend-apispecification-ib" = "nowa wersja"
                         writeJSON file: "package.json", json: jsonFile
                         //println(jbuilder)
-                        jsonFile = null
-*/
+                        //jsonFile = null
+                        
                     }
                }
             }
